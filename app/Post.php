e67e498801f201997bfa05ecf0d1dd2f181b4f38
curder <curder@foxmail.com>
2018-01-16 12:37:00 +0800

    typo fixed

--- app/Post.php
+++ app/Post.php
@@ -13,37 +13,37 @@ class Post extends Model
     protected static function boot()
     {
 
-        Post::creating(function($post){
+        static::creating(function($post){
             echo "creating event is fired\n";
             if($post->title == 'title') return false;
         });
 
-        Post::created(function($post){
+        static::created(function($post){
             echo "created event is fired\n";
         });
 
-        Post::updating(function($post){
+        static::updating(function($post){
             echo "updating event is fired\n";
         });
 
-        Post::updated(function($post){
+        static::updated(function($post){
             echo "updated event is fired\n";
         });
 
-        Post::saving(function($post){
+        static::saving(function($post){
             echo "saving event is fired\n";
         });
         
         
-        Post::saved(function($post){
+        static::saved(function($post){
             echo "saved event is fired\n";
         });
 
-        Post::deleting(function($post){
+        static::deleting(function($post){
             echo "deleting event is fired\n";
         });
 
-        Post::deleted(function($post) {
+        static::deleted(function($post) {
             echo "deleted event is fired\n";
         });
     }
